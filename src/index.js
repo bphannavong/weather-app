7810763b0250da6bc6801006ba28010fd5b7d125
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-30 19:57:26 -0700

    Add basic api fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import getWeather from "./weather.js";
+
+// getWeather("London");
--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,15 @@
-async function getWeather(city) {}
+async function getWeather(city) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ecd26b108dfa2ef49786d327842bf4b9`,
+    {
+      mode: "cors",
+    }
+  );
+  const stats = await response.json();
+  console.log(stats);
+  return stats;
+}
+
+function processStats(stats) {
+  //take data from fetch and turn into display data
+}
