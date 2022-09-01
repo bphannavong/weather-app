4dcf423b5e2704999e8c10094fe9b37c00c76605
Billy Phannavong <billy.phannavong@gmail.com>
2022-09-01 12:43:28 -0700

    Add logic for submitting/validating

--- src/dom.js
+++ src/dom.js
@@ -2,7 +2,9 @@ import events from "./mediator.js";
 
 import("./mediator.js");
 
+//cache DOM
 const city = document.getElementById("city");
+const country = document.getElementById("country");
 const sky = document.getElementById("sky");
 const temp = document.getElementById("temp-main");
 const feelsLike = document.getElementById("feels-like");
@@ -12,23 +14,43 @@ const humidity = document.getElementById("humidity");
 
 function updateDOM(stats) {
   city.innerHTML = stats.city;
+  country.innerHTML = stats.country;
   sky.innerHTML = stats.sky;
   temp.innerHTML = stats.temp;
-  feelsLike.innerHTML = stats.feelsLike;
-  tempHigh.innerHTML = stats.tempHigh;
-  tempLow.innerHTML = stats.tempLow;
-  humidity.innerHTML = stats.humidity;
+  feelsLike.innerHTML = `Feels like: ${stats.feelsLike}`;
+  tempHigh.innerHTML = `Today's High: ${stats.tempHigh}`;
+  tempLow.innerHTML = `Today's Low: ${stats.tempLow}`;
+  humidity.innerHTML = `Humidity: ${stats.humidity}%`;
 }
 
+//sends query to fetch
 function searchWeather() {
   const query = document.getElementById("query").value;
   events.publish("cityChanged", query);
 }
 
+//functions to convert units F / C
+
+//submits form on enter
+function checkEnter(e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    submit.click();
+  }
+}
+
+function showError(err) {
+  alert(err);
+}
+//bind events
 const submit = document.getElementById("submit");
 submit.addEventListener("click", searchWeather);
 
+const query = document.getElementById("query");
+query.addEventListener("keypress", checkEnter);
 events.subscribe("weatherStats", updateDOM);
+
+events.subscribe("notFound", showError);
 /* function button {
     gets update that button class was changed
     sends message to change units of dom / stats
--- src/weather.js
+++ src/weather.js
@@ -6,15 +6,17 @@ async function processStats(stats) {
   //take data from fetch and turn into display data
   const sky = stats.weather[0].description;
   const city = stats.name;
-  const temp = stats.main.temp;
-  const feelsLike = stats.main.feels_like;
-  const tempHigh = stats.main.temp_max;
-  const tempLow = stats.main.temp_min;
+  const country = stats.sys.country;
+  const temp = Math.round(stats.main.temp);
+  const feelsLike = Math.round(stats.main.feels_like);
+  const tempHigh = Math.round(stats.main.temp_max);
+  const tempLow = Math.round(stats.main.temp_min);
   const humidity = stats.main.humidity;
 
-  return await {
+  return {
     sky,
     city,
+    country,
     temp,
     feelsLike,
     tempHigh,
@@ -24,19 +26,21 @@ async function processStats(stats) {
 }
 
 async function getStats(city) {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ecd26b108dfa2ef49786d327842bf4b9&units=imperial`,
-    {
-      mode: "cors",
-    }
-  );
-  const stats = await response.json();
-  console.log(stats);
-  const processedStats = processStats(stats);
-  processedStats.then((data) => {
-    console.log(data);
-    events.publish("weatherStats", data);
-  });
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ecd26b108dfa2ef49786d327842bf4b9&units=imperial`,
+      {
+        mode: "cors",
+      }
+    );
+    const stats = await response.json();
+    console.log(stats);
+    const processedStats = await processStats(stats);
+    console.log(processedStats);
+    events.publish("weatherStats", processedStats);
+  } catch {
+    events.publish("notFound", "No matching city found");
+  }
 }
 
 events.subscribe("cityChanged", getStats);
